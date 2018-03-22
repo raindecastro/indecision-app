5b5b7e45d46fe354465cfbf2faad0122c3f2bfec
unknown <rainvincentdecastro@gmail.com>
2018-03-23 01:16:58 +0800

    Test of new code, 2nd commit/push

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
+console.log("LOL")
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
