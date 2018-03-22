c7fd674a309c45e11cb917063d9ae6cb192decf5
unknown <rainvincentdecastro@gmail.com>
2018-03-23 01:31:56 +0800

    Third Test for VSCode

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 console.log("LOL")
-
+console.log("TEST AGAIN");
+const template = (
+    <div>
+    </div>
+);
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
